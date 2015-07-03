cadcfb4525f1ffb51e5e2044583a84a3f966cd0e
Luigi Vincent <luigipvincent@gmail.com>
2015-07-03 12:28:01 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+#Tic Tac Toe game
+Features:
+Toggle & Reset capable Score Display.
+Adding in player name(s).
+
+Part of a series of simple apps made for the sake of learning.
+
+Try it out: [TicTacToe.jar](https://github.com/Javaliant/TicTacToe/blob/master/TicTacToe.jar?raw=true) 
+
+* **Requires at least JRE 8**
---
+++ TicTacToeSquare.java
@@ -0,0 +1,40 @@
+/* Author: Luigi Vincent
+
+*/
+
+import javafx.scene.control.Button;
+
+public class TicTacToeSquare {
+	private boolean filled;
+	static boolean turnTracker;
+	private Button square = new Button();
+	private final int SQUARE_SIZE = 100;
+
+	TicTacToeSquare() {
+		square.setMinHeight(SQUARE_SIZE);
+		square.setMinWidth(SQUARE_SIZE);
+		square.setOnAction(e -> {
+			if (square.getText().isEmpty()) {
+				square.setText(turnTracker ? "O" : "X");
+				square.setStyle(turnTracker ? "-fx-text-fill: gold;" : "-fx-text-fill: darkred;");
+				filled = true;
+				turnTracker = turnTracker ? false : true;
+				TicTacToe.evaluateBoard();
+			}
+		});
+	}
+
+	public Button button() {
+		return square;
+	}
+
+	public boolean equals(TicTacToeSquare target) {
+		return filled && square.getText().equals(target.button().getText());
+	}
+
+	public void clear() {
+		filled = false;
+		square.setText("");
+		square.setDisable(false);
+	}
+}
