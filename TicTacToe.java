03fdafc13090ed45bd62febbcbbd8cc57d528908
Luigi Vincent <luigipvincent@gmail.com>
2015-08-09 11:00:53 -0400

    comment update 3

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,6 @@
 /* Author: Luigi Vincent
 Main Class to run the tic tac toe game
-Created for learning/fun and as a step towards CodeReview's Ultimate Tic Tac Toe community challenge:
+Created for learning/fun and as a step towards CodeReview's Ultimate Tic Tac Toe community challenge.
 */
 
 import javafx.application.Application;
